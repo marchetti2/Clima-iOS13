79737983527f92977c775f86855981e8b49efd0a
Mário Alves <mario.alves@superopa.com>
2022-12-31 19:07:40 -0300

    add getData method

--- Clima/WeatherManager.swift
+++ Clima/WeatherManager.swift
@@ -9,10 +9,40 @@
 import Foundation
 
 struct WeatherManager {
-    let weatherURL = ""
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=8628c8bed635920d25f99064e88127b7&lang=pt&units=metric"
     
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
-        print(urlString)
+        getData(url: urlString)
     }
+    
+    func getData(url: String) {
+        
+        if let url = URL(string: url) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url, completionHandler: handler(data:response:error:))
+            
+            task.resume()
+            
+        }
+        
+    }
+    
+    func handler(data: Data?, response: URLResponse?, error: Error?) {
+        if error != nil {
+            print(error!)
+            return
+        }
+        
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString)
+        }
+    }
+    
+    
+    
+    
+    
 }
+
