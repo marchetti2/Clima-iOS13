c7270b40b36d6b4ff471f2f569622e886d79733e
Mário Alves <mario.alves@superopa.com>
2023-01-03 00:29:02 -0300

    Create a WeatherModel and understand Computed Properties

--- Clima/WeatherManager.swift
+++ Clima/WeatherManager.swift
@@ -41,8 +41,15 @@ struct WeatherManager {
         let decoder = JSONDecoder()
         do {
             let decodedData = try decoder.decode(WeatherData.self, from: data)
-            print(decodedData.main.temp)
-            print(decodedData.weather[0].description)
+            let id = decodedData.weather[0].id
+            let temp = decodedData.main.temp
+            let name = decodedData.name
+            
+            let weather = WeatherModel(conditionId: id, temperature: temp, cityName: name)
+            
+            print(weather.getConditionString)
+            print(weather.getTemperatureString)
+            
         } catch {
             print(error)
         }
